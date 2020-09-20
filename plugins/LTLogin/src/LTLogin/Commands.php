40b372d432319ff5071ff5d867821a582fd4c93a
TreeCraftNet <71262946+TreeCraft@users.noreply.github.com>
2020-09-20 17:20:15 +0800

    Update Commands.php

--- plugins/LTLogin/src/LTLogin/Commands.php
+++ plugins/LTLogin/src/LTLogin/Commands.php
@@ -21,23 +21,23 @@ class Commands{
 		}
 		switch(strtolower($args[0])){
 			case 'email':
-				if(isset($this->event->datas[$name]['email']) and $this->event->datas[$name]['email']!==NULL)return $Sender->sendMessage('§l§a[提示]§c你已经绑过邮箱了');return;
-				if(!isset($args[1]))return $Sender->sendMessage('§l§a[提示]§c用法：§d/login email §e邮箱地址');
+				if(isset($this->event->datas[$name]['email']) and $this->event->datas[$name]['email']!==NULL)return $Sender->sendMessage('§l§aLTCraft>>§c你已经绑过邮箱了');return;
+				if(!isset($args[1]))return $Sender->sendMessage('§l§aLTCraft>>§c用法：§d/login email §e邮箱地址');
 				if(preg_match('/^([0-9A-Za-z\\-_\\.]+)@([0-9a-z]+\\.[a-z]{2,3}(\\.[a-z]{2})?)$/i',$args[1])){
 					Server::getInstance()->dataBase->pushService('1'.chr(2)."update user set email='{$args[1]}' where name='{$name}'");
-					$Sender->sendMessage('§l§a[提示]§a绑定成功,你以后可以通过这个来找回密码　 &&　开发中．．');
+					$Sender->sendMessage('§l§aLTCraft>>§a绑定成功,你以后可以通过这个来找回密码　 &&　开发中．．');
 				}else{
-					$Sender->sendMessage('§l§a[提示]§c邮箱地址不合法');
+					$Sender->sendMessage('§l§aLTCraft>>§c邮箱地址不合法');
 				}
 			break;
 			case 'qq':
-				if(isset($this->event->datas[$name]['qq']) and $this->event->datas[$name]['qq']!==NULL)return $Sender->sendMessage('§l§a[提示]§c你已经绑过QQ了');return;
-				if(!isset($args[1]))return $Sender->sendMessage('§l§a[提示]§c用法：§d/login qq QQ号');
+				if(isset($this->event->datas[$name]['qq']) and $this->event->datas[$name]['qq']!==NULL)return $Sender->sendMessage('§l§aLTCraft>>§c你已经绑过QQ了');return;
+				if(!isset($args[1]))return $Sender->sendMessage('§l§aLTCraft>>§c用法：§d/login qq QQ号');
 				Server::getInstance()->dataBase->pushService('1'.chr(2)."update user set QQ='{$args[1]}' where name='{$name}'");
-				$Sender->sendMessage('§l§a[提示]§a绑定成功,你以后可以通过这个来找回密码 或查询');
+				$Sender->sendMessage('§l§aLTCraft>>§a绑定成功,你以后可以通过这个来找回密码 或查询');
 			break;
 			case 'resetp':
-				if(!isset($args[1])){$Sender->sendMessage('§l§a[提示]§c用法：§d/login resetp §e新密码');return;}
+				if(!isset($args[1])){$Sender->sendMessage('§l§aTCraft>>§c用法：§d/login resetp §e新密码');return;}
 				// $code=$this->sql->getCode($name);
 				// if($code!=false){
 					// if($code['code']==NULL){
@@ -47,12 +47,12 @@ class Commands{
 						// if($code['code']==$args[1]){
 							$check=$this->event->checkPassword($args[1]);
 							if($check!==true){
-								$Sender->sendMessage('§l§a[提示]§c'.$check);
+								$Sender->sendMessage('§l§aLTCraft>>§c'.$check);
 								return;
 							}
 							Server::getInstance()->dataBase->pushService('1'.chr(2)."update user set password='{$args[1]}' where name='{$name}'");
 							$this->event->datas[$name]['password']=$args[1];
-							$Sender->sendMessage('§l§a[提示]§a修改成功！');
+							$Sender->sendMessage('§l§aLTCraft>>§a修改成功！');
 						// }else{
 							// $Sender->sendMessage('§l§a[提示]§c验证码错误！');
 						// }
@@ -92,7 +92,7 @@ class Commands{
 				// $Sender->sendMessage('§d/login send §a向你的邮箱发送验证码');
 			break;
 			default:
-				$Sender->sendMessage('§l§a[提示]§c未知命令 输入§a/login help §c获取帮助列表');
+				$Sender->sendMessage('§l§aLTCraft>>§c未知命令 输入§a/login help §c获取帮助列表');
 			break;
 		}
 	}
